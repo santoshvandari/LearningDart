5807c3985578a3e3946e639fe0d717b675a333a0
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-15 22:06:23 +0545

    Initial

---
+++ Datatypes.dart
@@ -0,0 +1,13 @@
+void main(){
+    // Numbers,Strings, Boleans, Maps, Lists, Dynamic
+
+    String name = "Santosh";
+    print("First Number : $name");
+
+    int num=12;
+    print("Integer : $num");
+
+
+    double number = 325.45;
+    print("Dyanmice : $number");
+}
