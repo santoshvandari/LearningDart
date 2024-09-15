b142b2b011208dd0a20b74a5becfc95327a7a5e8
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-15 22:03:00 +0545

    Updated

---
+++ constandfinal.dart
@@ -0,0 +1,6 @@
+void main(){
+    const String name="Santosh Bandari"; //when you are cleared about the datatype 
+    final String nam="Data"; //When you are not fixed about the datatypes. 
+    print(name);
+    print(nam);
+}
--- variables1.dart
+++ variables1.dart
@@ -2,8 +2,30 @@ void main(){
     // varibles
 
     // Any kind of datatypes
+    print("Any Datatypes");
     var data=11;
-    String name="Santosh";
-    
+    var names="Bhandari";
+    var f=3.14;
+
+    // You can you dynamice also instaead of var
+    dynamic data1=11;
+    dynamic names1="Bhandari";
+    print(data);
+    print(names);
+    print(3.14);
+
+    // Integer Data Type
+    print("Integer Datatypes");
+    int a=10;
+    int b=20;
+    print(a+b);
+
+    // String Data Types
+    print("String Datatypes");
+    String name="Santosh Bhandari";
+    String address="Kanakai-07";
+    print(name);
+    print(address);
+
 
 }
