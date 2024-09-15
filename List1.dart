9bed1769ee3795bf0fb760c9cba33a7acb51d3e5
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-15 22:09:50 +0545

    Initial

---
+++ List1.dart
@@ -0,0 +1,12 @@
+void main(){
+    dynamic names=["santosh","Bhandari"];
+    print(names); 
+    print(names[0]); 
+    names.add("Adi");
+    names.addAll(["singh","Krish"]);
+    print(names); 
+
+
+
+
+}
