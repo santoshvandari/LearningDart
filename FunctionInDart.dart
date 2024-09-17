6b601097207163706f618d27ff783ee35f7c6764
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-17 19:40:59 +0545

    Factorial

--- FunctionInDart.dart
+++ FunctionInDart.dart
@@ -1,3 +1,14 @@
+int Factorial(int num){
+    int fact=1;
+    while(num!=0){
+        fact=fact*num;
+        num--;
+    }
+    return fact;
+}
+
+
 void main(){
-    
+    var result=Factorial(5);
+    print("Factorial of 10 is : $result");
 }
