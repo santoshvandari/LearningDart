b638bb3f9268ddb0f0bca134c995797652290f16
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-17 19:37:36 +0545

    Logics

---
+++ logiccase.dart
@@ -0,0 +1,18 @@
+void main(){
+    // if, if/else, if else if 
+    
+    var num=5;
+    if(num==3){
+        print("Number is 3");
+    }else if(num==1){
+        print("number is 1");
+    }else if (num==4){
+        print("Number is 4");
+    }else if (num==5){
+        print("Number is 5");
+    }else{
+        print("noting ");
+    }
+    
+    
+}
--- tempCodeRunnerFile.dart
+++
@@ -1,4 +0,0 @@
-    // dynamic datas=["santosh","Adi","Krishna","Kritish","Nayan"];
-    // for(dynamic data in datas){
-    //     print(data);
-    // }
