fc20e575ac93a1749f5eb1eaf4be5c7840b89fba
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-17 19:28:30 +0545

    ForLoops

---
+++ LoopsDart1.dart
@@ -0,0 +1,27 @@
+void main(){
+    // // For Loop 
+    // for(int i =0;i<10;i++){
+    //     print(i);
+    // }
+
+
+    // For In Loops
+    // dynamic datas=["santosh","Adi","Krishna","Kritish","Nayan"];
+    // for(dynamic data in datas){
+    //     print(data);
+    // }
+
+
+
+    // While Loop
+    int num=1;
+    while(num<=10){
+        print(num);
+        num++;
+    }
+
+
+
+
+
+}
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,4 @@
+    // dynamic datas=["santosh","Adi","Krishna","Kritish","Nayan"];
+    // for(dynamic data in datas){
+    //     print(data);
+    // }
