9f4f1fec2ace4f04eb022d120bffd6d7bb02420d
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-17 19:21:19 +0545

    MAps

--- mardemo.dart
+++ mardemo.dart
@@ -1,3 +1,12 @@
 void main(){
-    
+    var studentdata={
+        "name":"Santosh Bhandari",
+        "Address" : "Kanakai",
+        "Age" : 26,
+        "Course" : "BCA"
+    };
+    print(studentdata);
+    print(studentdata["name"]);
+    print(studentdata["Course"]);
+
 }
