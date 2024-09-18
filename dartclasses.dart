be422bdbc4cf1f39db530e325810436c4fbe4649
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-18 09:46:56 +0545

    Object Oriented Programming

--- dartclasses.dart
+++ dartclasses.dart
@@ -1,18 +1,23 @@
+// Creating the Class
 class Person{
     String? name,address;
     int? age;
+    // Defining the Constructor
     Person(String name,String address,int age){
         this.name=name;
         this.address=address;
         this.age=age;
     }
+    // Displaying the Result
     void Display(){
         print("Name : $name\nAge : $age\nAddress : $address");
     }
 }
 
-
+// Main Program
 void main(){
+    // Creating the Object
     Person obj = Person("Santosh Bandari","Kanakai-03",24);
+    // Invoking the methods
     obj.Display();
 }
