9b19866a40064e911f9d48cbbfa370c57f7e44ba
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-18 09:15:17 +0545

    Updated

---
+++ TypeCasting.dart
@@ -0,0 +1,6 @@
+void main(){
+    String num="10";
+    int a=10;
+    int b=int.parse(num);
+    print("sum of $a and $b is ${a+b}.");
+}
--- UserInput.dart
+++ UserInput.dart
@@ -1,21 +1,13 @@
-import 'dart.io';
+import 'dart:io';
 
-int factorial(int n){
-    int fact=1;
-    for(int i=1;i<=n;i++){
-        fact=fact*i;
-    }
-    return fact;
-}
+
 void main(){
     // User Input
 
-    print("Enter a Number: ");
-
-    var num=stdin.readLineSync();
-    // Converting the String into int
-    num=Integer.parseInt(num);
+    print("Enter You Name: ");
+ 
     
-    print("Factorial of $num is : ${factorial(num)}");
-
+   var name=stdin.readLineSync();
+   print("Hello $name");
+    
 }
