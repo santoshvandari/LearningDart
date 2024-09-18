c2ac11ca5c2a50b908fca415e0317827add163f2
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-18 09:18:21 +0545

    Updated

--- TypeCasting.dart
+++ TypeCasting.dart
@@ -3,4 +3,14 @@ void main(){
     int a=10;
     int b=int.parse(num);
     print("sum of $a and $b is ${a+b}.");
+
+
+
+    String number = a.toString();
+    print(number);
+
+
+    String value="0.125";
+    double val=double.parse(value);
+    print((a+val));
 }
