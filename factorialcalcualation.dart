95aab62ed699043215a53a9f46f8705ac76b9818
Santosh Bhandari <santoshvandari1@gmail.com>
2024-09-18 09:28:03 +0545

    FactorialCalculation

--- factorialcalcualation.dart
+++ factorialcalcualation.dart
@@ -1,7 +1,30 @@
-int factorial(int n){
-    int fact=1;
-    for(int i=1;i<=n;i++){
-        fact=fact*i;
-    }
-    return fact;
+import 'dart:io';
+
+int factorial(int n) {
+  int fact = 1;
+  for (int i = 1; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
+}
+
+bool isNumeric(String s) {
+  if (s == null) {
+    return false;
+  }
+  return int.tryParse(s) != null;
+}
+
+void main() {
+  print("Enter a Number: ");
+  var input = stdin.readLineSync();
+
+  if (input != null && input.isNotEmpty && isNumeric(input)) {
+    var n = int.parse(input);
+    var factorialValue = factorial(n);
+    print("Factorial of $n is $factorialValue.");
+  } else {
+    print("Number cannot be empty or a non-numeric value.");
+    main();
+  }
 }
